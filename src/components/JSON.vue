cc8f63d2598e223d819d0667f02caa86666f4e92
Brenda Dang <bdan0005@student.monash.edu>
2025-08-07 18:45:48 +1000

    Fix activity 4 and 5 to use find

--- src/components/JSON.vue
+++ src/components/JSON.vue
@@ -110,25 +110,25 @@ const showMessage = ref(false)
 // Activity 2: Get authors born after 1850
 const modernAuthors = computed(() => {
   // TODO: CODE TO FILTER ARRAY OF AUTHORS HERE
-  authors.filter((author) => author.birthYear > 1850)
+  return authors.filter((author) => author.birthYear > 1850)
 })
 
 // Activity 3: Get all famous works
 const allFamousWorks = computed(() => {
   // TODO: CODE TO GET ALL FAMOUS WORKS HERE
-  authors.flatMap((author) => author.famousWorks.map((work) => work.title))
+  return authors.flatMap((author) => author.famousWorks.map((work) => work.title))
 })
 
 // Activity 4: Find author by name
 const orwell = computed(() => {
   // TODO: CODE TO FIND AUTHOR BY NAME HERE
-  authors.filter((author) => author.name == "George Orwell")
+  return authors.find((author) => author.name === "George Orwell")
 })
 
 // Activity 5: Find author by ID
 const austen = computed(() => {
   // TODO: CODE TO FIND AUTHOR BY ID HERE
-  authors.filter((author) => author.id == 1)
+  return authors.find((author) => author.id == 1)
 })
 </script>
 
